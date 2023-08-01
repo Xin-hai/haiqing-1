a650dc0c99b3ab57aea318bfbb6410aa1822df9a
Xin-hai <aa1940358283@163.com>
2023-08-01 09:18:18 +0800

    创建custom.d.ts封装model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,24 +14,20 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+import model from '@/model.ts';
 
 
-const recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]')
-type Record = {
-  tags: string[]
-  notes: string
-  type: string
-  amount: number
-  createdAt: Date | undefined
-}
+const recordList = model.fetch()
+
+
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 
 export default class Money extends Vue {
   tags = ['服饰','餐饮','住宿','交通']
-  recordList: Record[] = recordList
-  record: Record = {
+  recordList: RecordItem[] = recordList
+  record: RecordItem = {
     tags: [], notes: '', type: '-',amount: 0, createdAt: new Date(2023,8,1)
   }
   onUpdateTags(value: string[]){
@@ -41,14 +37,14 @@ export default class Money extends Vue {
     this.record.notes = value
   }
   savaRecord(){
-    const record2:Record = JSON.parse(JSON.stringify(this.record)) // 拷贝一份
+    const record2:RecordItem = model.clone(this.record)  // 拷贝一份
     record2.createdAt = new Date()
     this.recordList.push(record2)
   }
 
   @Watch('recordList')
   onRecordListChange(){
-    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+    model.save(this.recordList)
   }
 }
 
