59db9d88647304363fc8bcaefcfa1b2ab46f52a1
Xin-hai <aa1940358283@163.com>
2023-08-04 07:22:16 +0800

    应用day.js解决时间显示问题

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,8 +3,8 @@
       <Tabs :data-source="recordTypeList" :value.sync="type" class-prefix="type" />
      <Tabs :data-source="intervalList" :value.sync="interval" class-prefix="interval" />
         <ol>
-          <li v-for="(group, index) in result" :key="index" >
-            <h3 class="title">{{group.title}}</h3>
+          <li v-for="group in result" :key="group.title" >
+            <h3 class="title">{{beautify(group.title)}}</h3>
             <ol>
               <li v-for="item in group.items" :key="item['id']" class="record">
                 <span>{{tagString(item.tags)}}</span>
@@ -17,39 +17,34 @@
     </Layout>
 
 </template>
-<style lang="scss" scoped>
-%item {
-  padding: 8px 16px;
-  line-height: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.title {
-  @extend %item;
-}
-.record {
-  @extend %item;
-  background: #fff;
-}
-.item-notes {
-  margin-right: auto;
-  margin-left: 16px;
-  color: #999;
-  font-size: 14px;
-}
-</style>
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/consts/intervalList';
 import recordTypeList from '@/consts/recordTypeList';
+import dayjs from 'dayjs'
+
 
 @Component({
   components: {Tabs}
 })
+
 export default class Statistics extends Vue {
+  beautify(string: string){
+    const day = dayjs(string)
+    const now = dayjs()
+    if(day.isSame(now,'day')){
+      return '今天'
+    } else if(day.isSame(now.subtract(1, 'day'),'day')){
+      return '昨天'
+    }else if(day.isSame(now.subtract(2, 'day'),'day')){
+      return '前天'
+    }else if(day.isSame(now,'year')){
+      return day.format('M月DD日')
+    }
+  return day.format('YYYY年M月DD日')
+  }
   tagString(tags: Tag[]){
     return tags.length === 0 ? '无' : tags.join(',')
   }
@@ -88,4 +83,29 @@ export default class Statistics extends Vue {
       height: 48px;
     }
   }
+%item {
+  padding-top: 8px;
+  padding-bottom: 8px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title {
+  @extend %item;
+  padding-left: 16px;
+}
+.record {
+  @extend %item;
+  background: #fff;
+  padding-left: 24px;
+  padding-right: 16px;
+}
+.item-notes {
+
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+  font-size: 14px;
+}
 </style>
