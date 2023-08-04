7553d6540133aa1a5bea06dfe38a937d54c70759
Xin-hai <aa1940358283@163.com>
2023-08-04 19:11:11 +0800

    PC端预览添加二维码，部署相关

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,18 @@ new Vue({
 }).$mount('#app');
 
 
+if(document.documentElement.clientWidth > 500){
+    window.alert('请使用手机打开此网站，以保证浏览效果')
+    const img = document.createElement('img')
+    img.src = '/qrcode.png'
+    img.style.position = 'fixed'
+    img.style.boxShadow = '0 0 10px rgba(0,0,0,0.5)'
+    img.style.left = '50%'
+    img.style.top = '50%'
+    img.style.transform = 'translate(-50%,-50%)'
+    document.body.appendChild(img)
+}
+
 // window.onload = function(){
 //     setTimeout(function(){
 //         window.scrollTo(0,10000)
